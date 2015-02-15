da71a0b5bcc86e63024127206e65d00f26bfe1d8
Espen Moe <espen357moe@gmail.com>
2015-02-15 17:58:10 +0100

    Lagde metode for å tegne skjermbildet, istedenfor å gjøre det rett i Paint-metoden til formen

--- csub/csub/Form1.cs
+++ csub/csub/Form1.cs
@@ -3,6 +3,7 @@ using System.Media;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
@@ -17,6 +18,10 @@ namespace csub
     public partial class Form1 : Form
     {
 
+        private bool running;
+        private readonly Form display;
+        public bool IsRunning { get { return running; } }
+        private readonly List<Torpedo> torpedoes;
         private Rectangle skyRectangle = new Rectangle(0, 0, 1000, 400);
         private Rectangle seaRectangle = new Rectangle(0, 400, 1000, 600);
 
@@ -33,29 +38,28 @@ namespace csub
         private void Form1_Paint(object sender, PaintEventArgs e)
         {     
     
-            Graphics g;
+           Graphics g;
             g = e.Graphics;
+            Render(g);
+            
+        }
+
+        //metode for å tegne selve skjermbildet
+        private void Render(Graphics g)
+        {
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
             LinearGradientBrush skyBrush = new LinearGradientBrush(skyRectangle, Color.DodgerBlue, Color.LightSkyBlue, 90);
             LinearGradientBrush seaBrush = new LinearGradientBrush(seaRectangle, Color.CornflowerBlue, Color.DarkBlue, 90);
-                         
+
             g.FillRectangle(skyBrush, 0, 0, 1000, 400);
             g.FillRectangle(seaBrush, 0, 400, 1000, 600);
-            
+
             Torpedo t = new Torpedo();
-            t.drawTorpedo(g, this.ClientSize);
+            t.Render(g);
 
             Image periskop = System.Drawing.Image.FromFile("../../images/periskop.png");
-            e.Graphics.DrawImage(periskop, 0, 0, (ClientSize.Width), ClientSize.Height);
-
-
-
+            g.DrawImage(periskop, 0, 0, (ClientSize.Width), ClientSize.Height);                            
         }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-
-        }
-
-        
-    }
+    }            
 }
