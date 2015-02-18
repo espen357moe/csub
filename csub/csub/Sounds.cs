680d3eaca14a207fbf9b6ea1c19097681d5a6f0a
Lorrides <tarhh@outlook.com>
2015-02-18 14:55:25 +0100

    Lagt til lyd for eksplosjon

---
+++ csub/csub/Sounds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace csub
+{
+    class Sounds
+    {
+
+        static void playExplosionSound (){
+            SoundPlayer player = new SoundPlayer("../../sounds/explosion.wav");
+        }
+
+
+    }
+}
