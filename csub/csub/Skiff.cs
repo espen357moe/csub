a4ff8a57107ae9509e325d4dc5ffc35f3fc3ac78
Espen Moe <espen357moe@gmail.com>
2015-02-19 12:02:38 +0100

    Eksperimenterte litt med direction

--- csub/csub/Skiff.cs
+++ csub/csub/Skiff.cs
@@ -10,10 +10,10 @@ namespace csub
     public abstract class Skiff : Boat
     {
         public override float Speed { get { return 1f; } }     
-        public bool Direction { get { return Form1.Direction.Left; }}
-        public float Distance { get { return 200f; } }
+        public override bool Direction { get { return Direction.Left; }}
+        public override float Distance { get { return 200f; } }
        
-        private static readonly Image pirateImage = Image.FromFile("foo.png");
+        private static readonly Image pirateImage = Image.FromFile("../../images/southpark.png");
        
         public static Image Image { return pirateImage; }
     }
