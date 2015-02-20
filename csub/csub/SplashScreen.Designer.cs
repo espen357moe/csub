916e691b5dc9fd684f273edbae9709f55f9707b5
Espen Moe <espen357moe@gmail.com>
2015-02-20 08:53:31 +0100

    Exit-knappen funker nå

--- csub/csub/SplashScreen.Designer.cs
+++ csub/csub/SplashScreen.Designer.cs
@@ -51,9 +51,9 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(670, 575);
+            this.richTextBox1.Location = new System.Drawing.Point(641, 545);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(300, 168);
+            this.richTextBox1.Size = new System.Drawing.Size(329, 198);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -75,11 +75,12 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(783, 555);
+            this.label1.Location = new System.Drawing.Point(778, 522);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 17);
             this.label1.TabIndex = 4;
