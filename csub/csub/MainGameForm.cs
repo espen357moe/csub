c8414baad67bd4554d8c74b6fbcaacfe43f92e16
Espen Moe <espen357moe@gmail.com>
2015-02-22 16:53:07 +0100

    Litt klarer konsollutskrift ved båtinstansiering

--- csub/csub/MainGameForm.cs
+++ csub/csub/MainGameForm.cs
@@ -143,7 +143,7 @@ namespace csub
                 theSkiff.Speed = (rnd.Next(1,5));
                 theSkiff.Distance = (rnd.Next(1,10));
                 boats.Add(theSkiff);
-                System.Console.WriteLine("Skiff added. Speed: " +theSkiff.Speed + ", distance" + theSkiff.Distance);
+                System.Console.WriteLine("Skiff added. Speed: " +theSkiff.Speed + ", distance: " + theSkiff.Distance);
                 SoundPlayer player = new SoundPlayer("../../sounds/cartmanrespect.wav");
                 player.Play(); 
             }
