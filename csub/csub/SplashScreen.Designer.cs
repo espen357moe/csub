ea0a6585e2843b4e935e2030ccf3353c795bf4dc
Lorrides <tarhh@outlook.com>
2015-02-23 22:46:06 +0100

    Lagt til lesing av XML score

--- csub/csub/SplashScreen.Designer.cs
+++ csub/csub/SplashScreen.Designer.cs
@@ -41,9 +41,10 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.ImageLocation = "";
-            this.pictureBox1.Location = new System.Drawing.Point(-9, -1);
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1000, 540);
+            this.pictureBox1.Size = new System.Drawing.Size(750, 439);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -51,17 +52,20 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(641, 545);
+            this.richTextBox1.Location = new System.Drawing.Point(481, 443);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(329, 198);
+            this.richTextBox1.Size = new System.Drawing.Size(248, 162);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 545);
+            this.button1.Location = new System.Drawing.Point(9, 443);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(516, 101);
+            this.button1.Size = new System.Drawing.Size(387, 82);
             this.button1.TabIndex = 2;
             this.button1.Text = "Play!";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,9 +73,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 652);
+            this.button2.Location = new System.Drawing.Point(9, 530);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(516, 91);
+            this.button2.Size = new System.Drawing.Size(387, 74);
             this.button2.TabIndex = 3;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
@@ -80,24 +85,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(778, 522);
+            this.label1.Location = new System.Drawing.Point(584, 424);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "High score";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // SplashScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(982, 755);
+            this.ClientSize = new System.Drawing.Size(736, 613);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SplashScreen";
             this.Text = "Das C# Boot";
             this.Load += new System.EventHandler(this.SplashScreen_Load);
--- csub/csub/SplashScreen.cs
+++ csub/csub/SplashScreen.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
+using System.Xml;
 
 namespace csub
 {
@@ -22,7 +23,46 @@ namespace csub
         {
               
             SoundPlayer player = new SoundPlayer("../../sounds/SOUNDSCRATE-DarkTexture.wav");  
-            player.Play();  
+            player.Play();
+
+
+            // Leser Scores fra XML
+
+            String s = "";
+            richTextBox1.Text = "";
+            try
+            {
+                XmlTextReader leser = new XmlTextReader("scores.xml");
+                while (leser.Read())  // leser neste
+                {
+                    switch (leser.NodeType)
+                    {
+                        case (XmlNodeType.Element):
+                            {
+                                //   s = s + leser.Name;
+                                if (leser.HasAttributes)
+                                {
+                                    for (int i = 0; i < leser.AttributeCount; i++)
+                                    {
+                                        s = s + "Name: " + leser.GetAttribute(i) + " ";
+                                    }
+                                }
+                                break;
+                            }
+                        case (XmlNodeType.Text):
+                            {
+                                s = s + "Poeng: " + leser.Value + "\n";
+                                break;
+                            }
+                    }
+                    richTextBox1.Text = s;
+                }
+                leser.Close();
+            }
+            catch (Exception f)
+            {
+            }
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -44,6 +84,12 @@ namespace csub
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+           
         }
     }
 }
