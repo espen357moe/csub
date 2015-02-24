26939f2a0be90e4b96be70f7c98579ce7e4167f1
Espen Moe <espen357moe@gmail.com>
2015-02-24 21:13:21 +0100

    Fjernet ubrukt skrot

--- csub/csub/SplashScreen.cs
+++ csub/csub/SplashScreen.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
-using System.Xml;
 
 namespace csub
 {
@@ -24,45 +16,6 @@ namespace csub
               
             SoundPlayer player = new SoundPlayer("../../sounds/SOUNDSCRATE-DarkTexture.wav");  
             player.Play();
-
-
-            // Leser Scores fra XML
-
-            String s = "";
-            richTextBox1.Text = "";
-            try
-            {
-                XmlTextReader leser = new XmlTextReader("../../scores/scores.xml");
-                while (leser.Read())  // leser neste
-                {
-                    switch (leser.NodeType)
-                    {
-                        case (XmlNodeType.Element):
-                            {
-                                //   s = s + leser.Name;
-                                if (leser.HasAttributes)
-                                {
-                                    for (int i = 0; i < leser.AttributeCount; i++)
-                                    {
-                                        s = s + "Name: " + leser.GetAttribute(i) + " ";
-                                    }
-                                }
-                                break;
-                            }
-                        case (XmlNodeType.Text):
-                            {
-                                s = s + "Poeng: " + leser.Value + "\n";
-                                break;
-                            }
-                    }
-                    richTextBox1.Text = s;
-                }
-                leser.Close();
-            }
-            catch (Exception f)
-            {
-            }
-
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -88,8 +41,7 @@ namespace csub
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-
-           
+         
         }
     }
 }
